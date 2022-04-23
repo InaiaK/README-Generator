aeac68aa3231b0fe9ff3afdf38288d937a49a6a6
InaiaKenia <inaiakenia15@gmail.com>
2022-04-23 14:20:14 -0600

    Added licenses links

--- index.js
+++ index.js
@@ -35,9 +35,10 @@ inquirer.prompt([
             name: 'license',
             message: 'What kind of license should your project have?',
             choices: [
-                "MIT",
-                "APACHE 2.0",
-                "GPL 3.0", 
+                "MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+                "APACHE 2.0 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+                "GPL 3.0 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", 
+                "BOOST [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
                 "None"
             ],
         },
